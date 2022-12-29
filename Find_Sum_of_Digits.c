e25633cc705893c038588039439f6d0054a4a8bb
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-12-29 10:32:34 +0530

    Add files via upload

---
+++ Find_Sum_of_Digits.c
@@ -0,0 +1,38 @@
+/*
+
+Write a program which accepts number from the user the return the sum of digit of that number.
+
+Input : 108
+Ouput : 9
+
+*/
+
+#include<stdio.h>
+
+int sumDigits(int iNum)
+{
+    int iSum = 0, iDigit = 0;
+
+    while(iNum != 0)
+    {   
+        iDigit = iNum % 10;
+        iSum += iDigit;
+        iNum = iNum / 10;
+    }
+
+    return iSum;
+}
+
+int main(void)
+{
+    int iNo = 0, iRet = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d", &iNo);
+
+    iRet = sumDigits(iNo);
+
+    printf("\nSum of the digits is : %d\n", iRet);
+
+    return 0;
+}
